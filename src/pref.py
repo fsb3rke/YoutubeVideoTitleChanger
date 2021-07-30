a6dada3c64ef89b54d1dc6e0e5447660cfbd5b7f
Berkee <berkeeavcii@gmail.com>
2021-07-30 11:15:09 +0300

    YoutubeVideoTitler

--- src/pref.py
+++ src/pref.py
@@ -1,6 +1,12 @@
 import json
 with open('pref.json') as f:
     data = json.load(f)
-username = str(data["bilgiler"]["user"])
-password = str(data["bilgiler"]["pass"])
-video = str(data["bilgiler"]["video"])
+username = ""
+password = ""
+video = ""
+for name in data["bilgiler"]:
+    username = name["user"]
+for passw in data["bilgiler"]:
+    password = passw["pass"]
+for vidw in data["bilgiler"]:
+    video = vidw["video"]
