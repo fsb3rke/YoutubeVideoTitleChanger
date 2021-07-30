cac79e11c239644a9959a33e1c0cad1b000614a4
Berkee <berkeeavcii@gmail.com>
2021-07-30 09:11:46 +0300

    YoutubeVideoTitler

--- src/browser.py
+++ src/browser.py
@@ -1,1 +1,11 @@
 from selenium import webdriver
+import time
+
+class Browser:
+    def __init__(self, link):
+        self.link = link
+        self.browser = webdriver.Chrome()
+        Browser.OpenBrowser(self)
+
+    def OpenBrowser(self):
+        self.browser.get(self.link)
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from browser import *
+
+Browser("https://www.youtube.com/")
+Browser.time.sleep(1000)
