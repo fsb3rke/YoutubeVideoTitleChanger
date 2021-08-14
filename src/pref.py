c5d484979e6bf1b6b35018967736d92d9b798a4b
Fsb3rKe <69055194+Fsb3rKe@users.noreply.github.com>
2021-08-14 14:11:19 +0300

    Update pref.py

--- src/pref.py
+++ src/pref.py
@@ -1,12 +1,12 @@
 import json
 with open('pref.json') as f:
     data = json.load(f)
-username = ""
-password = ""
-video = ""
-for name in data["bilgiler"]:
+username = data["bilgiler"]["user"]
+password = data["bilgiler"]["pass"]
+video = data["bilgiler"]["video"]
+"""for name in data["bilgiler"]:
     username = name["user"]
 for passw in data["bilgiler"]:
     password = passw["pass"]
 for vidw in data["bilgiler"]:
-    video = vidw["video"]
+    video = vidw["video"]"""
